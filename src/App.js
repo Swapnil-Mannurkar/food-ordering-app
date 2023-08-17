9d56e3329ba41769b015bfaebfbc4c951bde34c9
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-08-17 20:17:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cardIsShown, setCardIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCardIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCardIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cardIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from "react";
+import Input from "./Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  const amountInput = useRef();
+  const [amountIsValid, setAmountIsValid] = useState();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInput.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInput}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid number</p>}
+    </form>
+  );
+};
+
+export default MealItemForm;
