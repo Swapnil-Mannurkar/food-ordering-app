318c60fadb397a657f0080f2111799259ec015ce
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-08-17 20:19:17 +0530

    commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,6 +5,7 @@ import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  asd
 
   const totalAmount = cartCtx.totalAmount.toFixed(2);
   const hasItems = cartCtx.items.length > 0;
